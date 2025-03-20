1ea91d7f416cf3102240b5d542052374cf87540b
LiLi81323 <xiaoan_huang2023@126.com>
2025-03-20 17:39:13 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,6 +12,7 @@ function preload() {
 function setup() {
   createCanvas(1024, 798);
 
+
   playButton = createButton("Click to Play");
   playButton.position(width / 2 - 50, height / 2);
   playButton.mousePressed(startAudio);
@@ -34,35 +35,60 @@ function startAudio() {
   });
 }
 
+let smoothedBass = 0;
+let smoothedMid = 0;
+let smoothedTreble = 0;
+let smoothedTrunkThickness = 15;
+let highTreble = 0;
+let leafColorFactor = 0;
+
 function draw() {
   background(255);
   translate(width / 2, height);
-
-  if (!audioStarted) return; 
 
   let spectrum = fft.analyze();
 
   let bass = lerp(fft.getEnergy("bass"), fft.getEnergy("lowMid"), 0.5);
   let mid = lerp(fft.getEnergy("mid"), fft.getEnergy("highMid"), 0.5);
   let treble = fft.getEnergy("treble");
+  highTreble = fft.getEnergy(8000, 16000);
+  smoothedBass = lerp(smoothedBass, bass, 0.05);
+  smoothedMid = lerp(smoothedMid, mid, 0.05);
+  smoothedTreble = lerp(smoothedTreble, treble, 0.05);
 
-  let smoothedBass = lerp(0, bass, 0.05);
-  let smoothedMid = lerp(0, mid, 0.05);
-  let smoothedTreble = lerp(0, treble, 0.05);
+  let isHarshSound = highTreble > 120;
+
+  let targetThickness = map(
+    smoothedBass,
+    0,
+    255,
+    isHarshSound ? 5 : 15,
+    isHarshSound ? 10 : 30
+  );
+  smoothedTrunkThickness = lerp(smoothedTrunkThickness, targetThickness, 0.1);
 
   let branchAngle = map(smoothedMid, 0, 255, PI / 10, PI / 4);
   let leafOpacity = map(smoothedTreble, 0, 255, 100, 255);
 
-  recursiveBranch(180, 15, branchAngle, leafOpacity, 0);
+  if (isHarshSound) {
+    leafColorFactor = 1;
+  } else {
+    leafColorFactor = lerp(leafColorFactor, 0, 0.03);
+  }
+
+  recursiveBranch(180, smoothedTrunkThickness, branchAngle, leafOpacity, 0);
 }
 
 function recursiveBranch(len, weight, angle, leafAlpha, depth) {
   if (len < 10) return;
 
-  stroke(100, 50, 0);
+  let trunkColor = color(100, 50, 0);
+  stroke(trunkColor);
   strokeWeight(weight);
-  line(0, 0, 0, -len);
-  translate(0, -len);
+
+  let xOffset = random(-0.5, 0.5);
+  line(xOffset, 0, xOffset, -len);
+  translate(xOffset, -len);
 
   let angleOffset = branchAngles[depth % numBranches];
 
@@ -77,8 +103,23 @@ function recursiveBranch(len, weight, angle, leafAlpha, depth) {
   pop();
 
   if (len < 25) {
-    fill(34, 139, 34, leafAlpha);
+    let leafColor = color(
+      lerp(34, 255, leafColorFactor),
+      lerp(139, 204, leafColorFactor),
+      lerp(34, 0, leafColorFactor),
+      leafAlpha
+    );
+
+    fill(leafColor);
     noStroke();
-    ellipse(0, 0, random(5, 18), random(5, 18));
+
+    let leafSize = random(5, 18);
+    let leafCount = int(random(2, 6));
+
+    for (let i = 0; i < leafCount; i++) {
+      let x = random(-6, 6);
+      let y = random(-6, 6);
+      ellipse(x, y, leafSize, leafSize);
+    }
   }
 }
